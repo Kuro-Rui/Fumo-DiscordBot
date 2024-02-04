4ee4a1bf395c28fa5bca1a2f19b138d24ff3561d
Louis Dominic <louisdominic80@gmail.com>
2024-02-04 15:52:16 +0700

    Oopsies

--- core/events.py
+++ core/events.py
@@ -171,7 +171,7 @@ def init_events(bot: "FumoBot"):
             await ctx.send_help(ctx.command)
         elif isinstance(exception, MissingRequiredAttachment):
             await ctx.reply("You are missing a required attachment.")
-        elif isinstance(error, commands.UserInputError):
+        elif isinstance(exception, commands.UserInputError):
             await ctx.send_help()
         elif isinstance(exception, BadArgument):
             if isinstance(exception.__cause__, ValueError):
